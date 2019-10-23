d21fdd5eac85d38dd490d1e9dc4abe18e2b8fefe
Check Deng <checkdeng0903@gmail.com>
2019-10-23 20:20:39 +0800

    remove warning of scipy and numba, format code

--- utils.py
+++ utils.py
@@ -1,19 +1,28 @@
-import os, copy, glob, glob2, numpy as np, colorsys
+import os
+import copy
+import glob
+import glob2
+import numpy as np
+import colorsys
+
 
 def isstring(string_test):
-	try:
-		return isinstance(string_test, basestring)
-	except NameError:
-		return isinstance(string_test, str)
+    try:
+        return isinstance(string_test, basestring)
+    except NameError:
+        return isinstance(string_test, str)
+
 
 def islist(list_test):
-	return isinstance(list_test, list)
+    return isinstance(list_test, list)
+
 
 def islogical(logical_test):
-	return isinstance(logical_test, bool)
+    return isinstance(logical_test, bool)
+
 
 def isnparray(nparray_test):
-	return isinstance(nparray_test, np.ndarray)
+    return isinstance(nparray_test, np.ndarray)
 
 # def istuple(tuple_test):
 # 	return isinstance(tuple_test, tuple)
@@ -28,119 +37,157 @@ def isnparray(nparray_test):
 # 	'''
 # 	check if it is an extension, only '.something' is an extension, multiple extension is not a valid extension
 # 	'''
-# 	return isstring(ext_test) and ext_test[0] == '.' and len(ext_test) > 1 and ext_test.count('.') == 1
+# return isstring(ext_test) and ext_test[0] == '.' and len(ext_test) > 1
+# and ext_test.count('.') == 1
+
 
 def isinteger(integer_test):
-	if isnparray(integer_test): return False
-	try: return isinstance(integer_test, int) or int(integer_test) == integer_test
-	except (TypeError, ValueError): return False
+    if isnparray(integer_test):
+        return False
+    try:
+        return isinstance(integer_test, int) or int(integer_test) == integer_test
+    except (TypeError, ValueError):
+        return False
+
 
 def is_path_valid(pathname):
-	try:
-		if not isstring(pathname) or not pathname: return False
-	except TypeError: return False
-	else: return True
+    try:
+        if not isstring(pathname) or not pathname:
+            return False
+    except TypeError:
+        return False
+    else:
+        return True
+
 
 def is_path_creatable(pathname):
-	'''
-	if any previous level of parent folder exists, returns true
-	'''
-	if not is_path_valid(pathname): return False
-	pathname = os.path.normpath(pathname)
-	pathname = os.path.dirname(os.path.abspath(pathname))
+    '''
+    if any previous level of parent folder exists, returns true
+    '''
+    if not is_path_valid(pathname):
+        return False
+    pathname = os.path.normpath(pathname)
+    pathname = os.path.dirname(os.path.abspath(pathname))
 
-	# recursively to find the previous level of parent folder existing
-	while not is_path_exists(pathname):
-		pathname_new = os.path.dirname(os.path.abspath(pathname))
-		if pathname_new == pathname: return False
-		pathname = pathname_new
-	return os.access(pathname, os.W_OK)
+    # recursively to find the previous level of parent folder existing
+    while not is_path_exists(pathname):
+        pathname_new = os.path.dirname(os.path.abspath(pathname))
+        if pathname_new == pathname:
+            return False
+        pathname = pathname_new
+    return os.access(pathname, os.W_OK)
+
 
 def is_path_exists(pathname):
-	try: return is_path_valid(pathname) and os.path.exists(pathname)
-	except OSError: return False
+    try:
+        return is_path_valid(pathname) and os.path.exists(pathname)
+    except OSError:
+        return False
+
 
 def is_path_exists_or_creatable(pathname):
-	try: return is_path_exists(pathname) or is_path_creatable(pathname)
-	except OSError: return False
+    try:
+        return is_path_exists(pathname) or is_path_creatable(pathname)
+    except OSError:
+        return False
+
 
 def isfolder(pathname):
-	'''
-	if '.' exists in the subfolder, the function still justifies it as a folder. e.g., /mnt/dome/adhoc_0.5x/abc is a folder
-	if '.' exists after all slashes, the function will not justify is as a folder. e.g., /mnt/dome/adhoc_0.5x is NOT a folder
-	'''
-	if is_path_valid(pathname):
-		pathname = os.path.normpath(pathname)
-		if pathname == './': return True
-		name = os.path.splitext(os.path.basename(pathname))[0]
-		ext = os.path.splitext(pathname)[1]
-		return len(name) > 0 and len(ext) == 0
-	else: return False
+    '''
+    if '.' exists in the subfolder, the function still justifies it as a folder. e.g., /mnt/dome/adhoc_0.5x/abc is a folder
+    if '.' exists after all slashes, the function will not justify is as a folder. e.g., /mnt/dome/adhoc_0.5x is NOT a folder
+    '''
+    if is_path_valid(pathname):
+        pathname = os.path.normpath(pathname)
+        if pathname == './':
+            return True
+        name = os.path.splitext(os.path.basename(pathname))[0]
+        ext = os.path.splitext(pathname)[1]
+        return len(name) > 0 and len(ext) == 0
+    else:
+        return False
+
 
 def safe_path(input_path, warning=True, debug=True):
     '''
     convert path to a valid OS format, e.g., empty string '' to '.', remove redundant '/' at the end from 'aa/' to 'aa'
 
     parameters:
-    	input_path:		a string
+        input_path:		a string
 
     outputs:
-    	safe_data:		a valid path in OS format
+        safe_data:		a valid path in OS format
     '''
-    if debug: assert isstring(input_path), 'path is not a string: %s' % input_path
+    if debug:
+        assert isstring(input_path), 'path is not a string: %s' % input_path
     safe_data = copy.copy(input_path)
     safe_data = os.path.normpath(safe_data)
     return safe_data
 
+
 def fileparts(input_path, warning=True, debug=True):
-	'''
-	this function return a tuple, which contains (directory, filename, extension)
-	if the file has multiple extension, only last one will be displayed
+    '''
+    this function return a tuple, which contains (directory, filename, extension)
+    if the file has multiple extension, only last one will be displayed
 
-	parameters:
-		input_path:     a string path
+    parameters:
+            input_path:     a string path
 
-	outputs:
-		directory:      the parent directory
-		filename:       the file name without extension
-		ext:            the extension
-	'''
-	good_path = safe_path(input_path, debug=debug)
-	if len(good_path) == 0: return ('', '', '')
-	if good_path[-1] == '/':
-		if len(good_path) > 1: return (good_path[:-1], '', '')	# ignore the final '/'
-		else: return (good_path, '', '')	                          # ignore the final '/'
+    outputs:
+            directory:      the parent directory
+            filename:       the file name without extension
+            ext:            the extension
+    '''
+    good_path = safe_path(input_path, debug=debug)
+    if len(good_path) == 0:
+        return ('', '', '')
+    if good_path[-1] == '/':
+        if len(good_path) > 1:
+            return (good_path[:-1], '', '')  # ignore the final '/'
+        else:
+            # ignore the final '/'
+            return (good_path, '', '')
 
-	directory = os.path.dirname(os.path.abspath(good_path))
-	filename = os.path.splitext(os.path.basename(good_path))[0]
-	ext = os.path.splitext(good_path)[1]
-	return (directory, filename, ext)
+    directory = os.path.dirname(os.path.abspath(good_path))
+    filename = os.path.splitext(os.path.basename(good_path))[0]
+    ext = os.path.splitext(good_path)[1]
+    return (directory, filename, ext)
+
 
 def mkdir_if_missing(input_path, warning=True, debug=True):
-	'''
-	create a directory if not existing:
-		1. if the input is a path of file, then create the parent directory of this file
-		2. if the root directory does not exists for the input, then create all the root directories recursively until the parent directory of input exists
+    '''
+    create a directory if not existing:
+            1. if the input is a path of file, then create the parent directory of this file
+            2. if the root directory does not exists for the input, then create all the root directories recursively until the parent directory of input exists
 
-	parameters:
-		input_path:     a string path
-	'''
-	good_path = safe_path(input_path, warning=warning, debug=debug)
-	if debug: assert is_path_exists_or_creatable(good_path), 'input path is not valid or creatable: %s' % good_path
-	dirname, _, _ = fileparts(good_path)
-	if not is_path_exists(dirname): mkdir_if_missing(dirname)
-	if isfolder(good_path) and not is_path_exists(good_path): os.mkdir(good_path)
+    parameters:
+            input_path:     a string path
+    '''
+    good_path = safe_path(input_path, warning=warning, debug=debug)
+    if debug:
+        assert is_path_exists_or_creatable(
+            good_path), 'input path is not valid or creatable: %s' % good_path
+    dirname, _, _ = fileparts(good_path)
+    if not is_path_exists(dirname):
+        mkdir_if_missing(dirname)
+    if isfolder(good_path) and not is_path_exists(good_path):
+        os.mkdir(good_path)
+
 
 def load_txt_file(file_path, debug=True):
     '''
     load data or string from text file
     '''
     file_path = safe_path(file_path)
-    if debug: assert is_path_exists(file_path), 'text file is not existing at path: %s!' % file_path
-    with open(file_path, 'r') as file: data = file.read().splitlines()
+    if debug:
+        assert is_path_exists(
+            file_path), 'text file is not existing at path: %s!' % file_path
+    with open(file_path, 'r') as file:
+        data = file.read().splitlines()
     num_lines = len(data)
     file.close()
     return data, num_lines
+
 
 def load_list_from_folder(folder_path, ext_filter=None, depth=1, recursive=False, sort=True, save_path=None, debug=True):
     '''
@@ -158,15 +205,22 @@ def load_list_from_folder(folder_path, ext_filter=None, depth=1, recursive=False
         num_elem:       number of the elements
     '''
     folder_path = safe_path(folder_path)
-    if debug: assert isfolder(folder_path), 'input folder path is not correct: %s' % folder_path
+    if debug:
+        assert isfolder(
+            folder_path), 'input folder path is not correct: %s' % folder_path
     if not is_path_exists(folder_path):
         print('the input folder does not exist\n')
         return [], 0
     if debug:
-        assert islogical(recursive), 'recursive should be a logical variable: {}'.format(recursive)
-        assert depth is None or (isinteger(depth) and depth >= 1), 'input depth is not correct {}'.format(depth)
-        assert ext_filter is None or (islist(ext_filter) and all(isstring(ext_tmp) for ext_tmp in ext_filter)) or isstring(ext_filter), 'extension filter is not correct'
-    if isstring(ext_filter): ext_filter = [ext_filter]                               # convert to a list
+        assert islogical(
+            recursive), 'recursive should be a logical variable: {}'.format(recursive)
+        assert depth is None or (
+            isinteger(depth) and depth >= 1), 'input depth is not correct {}'.format(depth)
+        assert ext_filter is None or (islist(ext_filter) and all(isstring(
+            ext_tmp) for ext_tmp in ext_filter)) or isstring(ext_filter), 'extension filter is not correct'
+    if isstring(ext_filter):
+        # convert to a list
+        ext_filter = [ext_filter]
     # zxc
 
     fulllist = list()
@@ -178,32 +232,38 @@ def load_list_from_folder(folder_path, ext_filter=None, depth=1, recursive=False
                 # wildcard = os.path.join(wildcard_prefix, '*' + string2ext_filter(ext_tmp))
                 wildcard = os.path.join(wildcard_prefix, '*' + ext_tmp)
                 curlist = glob2.glob(os.path.join(folder_path, wildcard))
-                if sort: curlist = sorted(curlist)
+                if sort:
+                    curlist = sorted(curlist)
                 fulllist += curlist
         else:
             wildcard = wildcard_prefix
             curlist = glob2.glob(os.path.join(folder_path, wildcard))
-            if sort: curlist = sorted(curlist)
+            if sort:
+                curlist = sorted(curlist)
             fulllist += curlist
     else:                    # find files based on depth and recursive flag
         wildcard_prefix = '*'
-        for index in range(depth-1): wildcard_prefix = os.path.join(wildcard_prefix, '*')
+        for index in range(depth - 1):
+            wildcard_prefix = os.path.join(wildcard_prefix, '*')
         if ext_filter is not None:
             for ext_tmp in ext_filter:
                 # wildcard = wildcard_prefix + string2ext_filter(ext_tmp)
                 wildcard = wildcard_prefix + ext_tmp
                 curlist = glob.glob(os.path.join(folder_path, wildcard))
-                if sort: curlist = sorted(curlist)
+                if sort:
+                    curlist = sorted(curlist)
                 fulllist += curlist
             # zxc
         else:
             wildcard = wildcard_prefix
             curlist = glob.glob(os.path.join(folder_path, wildcard))
             # print(curlist)
-            if sort: curlist = sorted(curlist)
+            if sort:
+                curlist = sorted(curlist)
             fulllist += curlist
         if recursive and depth > 1:
-            newlist, _ = load_list_from_folder(folder_path=folder_path, ext_filter=ext_filter, depth=depth-1, recursive=True)
+            newlist, _ = load_list_from_folder(
+                folder_path=folder_path, ext_filter=ext_filter, depth=depth - 1, recursive=True)
             fulllist += newlist
 
     fulllist = [os.path.normpath(path_tmp) for path_tmp in fulllist]
@@ -212,14 +272,19 @@ def load_list_from_folder(folder_path, ext_filter=None, depth=1, recursive=False
     # save list to a path
     if save_path is not None:
         save_path = safe_path(save_path)
-        if debug: assert is_path_exists_or_creatable(save_path), 'the file cannot be created'
+        if debug:
+            assert is_path_exists_or_creatable(
+                save_path), 'the file cannot be created'
         with open(save_path, 'w') as file:
-            for item in fulllist: file.write('%s\n' % item)
+            for item in fulllist:
+                file.write('%s\n' % item)
         file.close()
 
     return fulllist, num_elem
 
-###################################################### visualization
+# visualization
+
+
 def random_colors(N, bright=True):
     """
     Generate random colors.
